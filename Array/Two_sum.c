d6e06ed8e34044a528b4457e2fb86bc6b5d9dbc2
Swarali Varghade <swaralivarghade05@gmail.com>
2026-05-17 13:22:02 +0530

    Update Two_sum.c

--- Array/Two_sum.c
+++ Array/Two_sum.c
@@ -4,7 +4,7 @@ int main(){
 	int num[10] = {1, 4, 5, 7, 9, 10, 13, 15};
 	int n = sizeof(num) / sizeof(num[0]);
 	int i = 0, j = n-1, k, target = 21;
-	while(i < j){
+	while(i < j){//Two pointer approach
 		if(num[i] + num[j] == target){
 			printf("%d + %d = %d",num[i], num[j], target);
 			return 0;
