f95a0c3f9bd106730085275ae3524656580c6cbf
SwaraliVarghade <swaralivarghade05@gmail.com>
2026-05-23 21:53:40 +0530

    Trapping Water problem

---
+++ Array/Trapping_water.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+	int height[] = {4,2,0,3,2,5};
+	int heightSize = sizeof(height)/sizeof(height[0]);
+	
+	int left = 0, right = heightSize-1, leftMax = 0, rightMax = 0, water = 0;
+	
+	while(left < right){
+		if(height[left] < height[right]){
+			if(height[left] >= leftMax){
+				leftMax = height[left];
+			}
+			else{
+				water += leftMax-height[left];
+			}
+			left++;
+		}else{
+			if(height[right] >= rightMax){
+				rightMax = height[right];
+			}
+			else{
+				water += rightMax-height[right];
+			}
+			right--;
+		}
+	}
+	printf("\nWater Trapped = %d", water);
+}
